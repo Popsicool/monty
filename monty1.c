f87ddd2320d9689d53ce9abef4e50926a4821486
Popsicool <akinolasamson1234@gmail.com>
2022-10-06 08:11:56 +0000

    betty

--- monty1.c
+++ monty1.c
@@ -1,4 +1,6 @@
 #include "monty.h"
+stack_t *head = NULL;
+
 /**
  * main - entry point.
  * @argc: arg count
@@ -8,7 +10,6 @@
 
 int main(int argc, char *argv[])
 {
-	stack_t *head = NULL;
 
 	if (argc != 2)
 	{
